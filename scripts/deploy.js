1090151d79aa9ac05aeb006215b90bb67e63034f
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-01-29 15:50:11 +0000

    Created a script that will run the methods provided by ethers.js
    in order to deploy Faucet.sol to the Göerli test network.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,37 @@
+const ethers = require("ethers");
+require("dotenv").config();
+
 async function main() {
-	const Lock = await hre.ethers.getContractFactory("Lock");
-	const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+	const url = process.env.ALCHEMY_TESTNET_RPC_URL;
 
-	await lock.deployed();
+	const provider = new ethers.providers.JsonRpcProvider(url);
 
-	console.log(
-		`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+	let artifacts = await hre.artifacts.readArtifact("Faucet");
+
+	//imported from my .env file using process.env
+	let privateKey = process.env.TESTNET_PRIVATE_KEY;
+
+	let wallet = new ethers.Wallet(privateKey, provider);
+
+	// Create an instance of a Faucet Factory
+	let factory = new ethers.ContractFactory(
+		artifacts.abi,
+		artifacts.bytecode,
+		wallet
 	);
+
+	let faucet = await factory.deploy();
+
+	console.log("Faucet address:", faucet.address);
+
+	await faucet.deployed();
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-	console.error(error);
-	process.exitCode = 1;
-});
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});
