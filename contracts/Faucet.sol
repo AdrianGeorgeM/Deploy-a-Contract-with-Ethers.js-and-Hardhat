852d25f83b2c9464d083d483820bc3643c6bc44d
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-01-29 15:46:35 +0000

    Added some small comments about Faucet contract ana what is doing
    The Faucet contract is quite simple;
    it allows anyone to call the withdraw method and specify an amount lower than .1 ETH at a time.
    The receive fallback function will handle any ETH deposited into the Faucet and add it to the contract balance.

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -1,14 +1,12 @@
-//SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
 contract Faucet {
-  
   function withdraw(uint _amount) public {
     // users can only withdraw .1 ETH at a time!
     require(_amount <= 100000000000000000);
     payable(msg.sender).transfer(_amount);
   }
-
   // fallback function
   receive() external payable {}
 }
